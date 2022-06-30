7fb662c2d6219ae9c9985f0104b1c1230d23e11c
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-06-30 23:13:53 +0530

    s=input() s=s.lower() a="abcdefghijklmnopqrstuvwxyz" for i in a:     if i not in s:         print(False)         break else:     print(True)

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,13 +1,9 @@
-str=input()
-alphabet="abcdefghijklmnopqrstuvwxyz"
-flag=0
-for c in alphabet:
-    if c not in str.lower():
-        flag=1
+s=input()
+s=s.lower()
+a="abcdefghijklmnopqrstuvwxyz"
+for i in a:
+    if i not in s:
+        print(False)
         break
-if(flag==0):
-    print("True")
 else:
-    print("False")
-        
-   
+    print(True)
