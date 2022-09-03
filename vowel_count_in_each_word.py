e0007381fea3eb266b2333356feef9fa376e4634
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-09-03 11:33:45 +0530

    a=list(map(str,input().split())) s="aeiouAEIOU" d=[] for i in a:     c=0     for j in i:         if j in s:             c+=1     d.append(c) for i in d:     print(i,end=' ')

--- vowel_count_in_each_word.py
+++ vowel_count_in_each_word.py
@@ -1,12 +1,11 @@
-l=list(map(str,input().split()))
+a=list(map(str,input().split()))
+s="aeiouAEIOU"
 d=[]
-for i in l:
-    count=0
+for i in a:
+    c=0
     for j in i:
-        if j in "aeiouAEIOU":
-            count=count+1
-    d.append(count)
+        if j in s:
+            c+=1
+    d.append(c)
 for i in d:
-    print(i,end=" ")
-    
-        
+    print(i,end=' ')
