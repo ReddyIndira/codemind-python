2a40a9ca742490b04797010611fd1ed39f7e9f82
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-09-12 21:09:22 +0530

    def prime(n):     if n==1:         return 0     elif n==2:         return 1     else:         for i in range(2,int(n**0.5)+2):             if n%i==0:                 return 1         return 0 n=int(input()) if(prime(n)==1):     print("not a prime") else:     print("prime")

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,15 @@
+def prime(n):
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        for i in range(2,int(n**0.5)+2):
+            if n%i==0:
+                return 1
+        return 0
 n=int(input())
-c=0
-for i in range(1,n+1):
-    if(n%i==0):
-        c=c+1   
-if (c==2):
-    print("prime")
-else:
+if(prime(n)==1):
     print("not a prime")
+else:
+    print("prime")
