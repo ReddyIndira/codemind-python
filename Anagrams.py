5e42727d438c48407264cb38e740583fa7d67a34
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-09-16 19:39:52 +0530

    s1=input() s2=input() c=[] for i in s1:     for i in s2:         if i in s1 and i in s2:             c.append(i) for i in c:     if(len(s1)==len(s2)):         print("True")         break     else:         print("False")         break

--- Anagrams.py
+++ Anagrams.py
@@ -1,11 +1,15 @@
 s1=input()
 s2=input()
-s1=s1.lower()
-s2=s2.lower()
+c=[]
 for i in s1:
-    if i not in s2:
+    for i in s2:
+        if i in s1 and i in s2:
+            c.append(i)
+for i in c:
+    if(len(s1)==len(s2)):
+        print("True")
+        break
+    else:
         print("False")
         break
-else:
-    print("True")
-        
+            
