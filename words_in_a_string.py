d8c3a35a4d5a34fdcf6bcc7b6cf5004731de6110
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-09-16 06:49:51 +0530

    s=input().split() for i in s:     print(len(s))     break

--- words_in_a_string.py
+++ words_in_a_string.py
@@ -1,9 +1,4 @@
-a=input()
-count=0
-for i in a:
-    if(i==" "):
-        count+=1
-if(count>0):
-    print(count+1)
-else:
-    print("1")
+s=input().split()
+for i in s:
+    print(len(s))
+    break
