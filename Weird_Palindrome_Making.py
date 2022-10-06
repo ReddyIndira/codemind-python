c902ebfa735821062d63bca196852d17111f231b
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-10-06 07:00:22 +0530

    T=int(input()) for i in range(T):     c=0     n=int(input())     a=list(map(int,input().split()))     s=len(a)     for j in range(0,s):         if a[j]%2!=0:             c+=1     print(c//2)

---
+++ Weird_Palindrome_Making.py
@@ -0,0 +1,11 @@
+T=int(input())
+for i in range(T):
+    c=0
+    n=int(input())
+    a=list(map(int,input().split()))
+    s=len(a)
+    for j in range(0,s):
+        if a[j]%2!=0:
+            c+=1
+    print(c//2)
+    
