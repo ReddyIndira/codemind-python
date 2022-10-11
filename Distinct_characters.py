5996603710a64a9f96cbadafdfa73d7fa139c1cd
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-10-11 23:23:24 +0530

    s=input().lower() t=[] for i in s:     if i not in t and i!=" ":         t.append(i) t=sorted(t) for i in t:     print(i,end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,9 +1,8 @@
 s=input().lower()
-b=[]
-for i in range(len(s)):
-    if(s.count(s[i])==1) and s[i]!=" ":
-        b.append(s[i])
-c=sorted(b)
-for i in c:
+t=[]
+for i in s:
+    if i not in t and i!=" ":
+        t.append(i)
+t=sorted(t)
+for i in t:
     print(i,end="")
-    
