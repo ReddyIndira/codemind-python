4a55f3c4d950a427d7cf7394f940e8d4193d1fd9
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-10-12 11:27:53 +0530

    s1=input().lower().split() s2=input().lower().split() c=[] for i in s2:     if i in s1 and i!=" ":         c.append(i) for i in c:     print(i,end=" ")

--- common_words.py
+++ common_words.py
@@ -1,10 +1,10 @@
 s1=input().lower().split()
 s2=input().lower().split()
 c=[]
-for i in s1:
-    if i in s2:
-        c.append(i)
 for i in s2:
-    if i in c:
-        print(i,end=' ')
+    if i in s1 and i!=" ":
+        c.append(i)
+for i in c:
+    print(i,end=" ")
+
         
