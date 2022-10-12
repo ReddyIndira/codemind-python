34e70259e9b18cbd44dda44b92f7a8f8030a3d9f
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-10-12 18:45:55 +0530

    s1=input().lower() s2=input().lower() c=[] d=0 for i in s1:     if i not in s2 and i not in c and i!=" ":         c.append(i)         d+=1 for i in s2:     if i not in s1 and i not in c and  i!=" ":         c.append(i)         d+=1 print(d)

--- Non_common_characters.py
+++ Non_common_characters.py
@@ -1,10 +1,15 @@
-s1=list(input().lower().replace(' ',''))
-s2=list(input().lower().replace(' ',''))
+s1=input().lower()
+s2=input().lower()
 c=[]
+d=0
 for i in s1:
-    if i not in s2 and i not in c:
+    if i not in s2 and i not in c and i!=" ":
         c.append(i)
+        d+=1
 for i in s2:
-    if i not in s1 and i not in c:
+    if i not in s1 and i not in c and  i!=" ":
         c.append(i)
-print(len(c))
+        d+=1
+print(d)
+
+        
