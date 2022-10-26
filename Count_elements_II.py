378f632227a1d75f9d0bd0477bda0f8a663a0b71
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-10-26 21:31:46 +0530

    a,b=map(int,input().split()) arr=list(map(int,input().split())) brr=list(map(int,input().split())) c=0 l=0 d=[] for i in arr:     if i not in brr and i not in d:         d.append(i)         c+=1 for i in brr:     if i not in arr and i not in d:         d.append(i)         l+=1 print(c+l)

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -1,13 +1,15 @@
-a,b=list(map(int,input().split()))
-h=list(map(int,input().split()))
-m=list(map(int,input().split()))
-l=set(h)
-n=set(m)
+a,b=map(int,input().split())
+arr=list(map(int,input().split()))
+brr=list(map(int,input().split()))
 c=0
-for i in l:
-    if i not in n:
+l=0
+d=[]
+for i in arr:
+    if i not in brr and i not in d:
+        d.append(i)
         c+=1
-for i in n:
-    if i not in l:
-        c+=1
-print(c)
+for i in brr:
+    if i not in arr and i not in d:
+        d.append(i)
+        l+=1
+print(c+l)
